b2b1e3af0cbb2ae02e9133ad095c27bd9f7bfae1
Sososhas <111687599+Sososhas@users.noreply.github.com>
2022-10-06 00:11:36 +0300

    Add files via upload

---
+++ point.h
@@ -0,0 +1,31 @@
+#ifndef POINT_H_INCLUDED
+#define POINT_H_INCLUDED
+
+
+class Point
+{
+
+private:
+    float mx, my;
+
+public:
+
+Point(float x, float y);
+Point();
+float getX() const;
+float getY() const;
+void setX(float x);
+void setY(float y);
+void normalize();
+float distance(const Point& p) const;
+float norm() const;
+Point operator+(const Point& right) const;
+Point operator*(float a) const;
+friend Point operator*(float a, const Point& p);
+friend std::ostream& operator<<(std::ostream& left, const Point& right);
+};
+
+
+
+
+#endif // POINT_H_INCLUDED
